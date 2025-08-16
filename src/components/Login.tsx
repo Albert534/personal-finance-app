bb500547a9f1dc52563eac0544c69de41f29b91e
Albert534 <crustingenius@gmail.com>
2025-08-16 23:46:18 +0630

    dashboard ui done

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,12 +1,10 @@
-import React , {useState} from 'react';
+import React, { useState } from 'react';
 import { TextInput, Button } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { Link } from '@tanstack/react-router';
-import { BsEye , BsEyeSlash } from 'react-icons/bs';
+import { BsEye, BsEyeSlash } from 'react-icons/bs';
 const Login = () => {
-
-
-		const [isEyeOpen , setIsEyeOpen] = useState(false);
+	const [isEyeOpen, setIsEyeOpen] = useState(false);
 	const form = useForm({
 		mode: 'uncontrolled',
 		initialValues: { password: '', email: '' },
@@ -39,12 +37,21 @@ const Login = () => {
 						<TextInput
 							label='Password'
 							placeholder='Password'
-							rightSection= {isEyeOpen ? <BsEye onClick={() => setIsEyeOpen(false)}  className='text-black' /> : <BsEyeSlash onClick={() => setIsEyeOpen(true) } className='text-black'/>}
-							type = {isEyeOpen ? 'text' : 'password'}
+							rightSection={
+								isEyeOpen ?
+									<BsEye
+										onClick={() => setIsEyeOpen(false)}
+										className='text-black cursor-pointer'
+									/>
+								:	<BsEyeSlash
+										onClick={() => setIsEyeOpen(true)}
+										className='text-black cursor-pointer'
+									/>
+							}
+							type={isEyeOpen ? 'text' : 'password'}
 							mt={'10px'}
 							key={form.key('password')}
-							{...form.getInputProps('password')
-}
+							{...form.getInputProps('password')}
 						/>
 						<div className='flex justify-between'>
 							<Link to='/forgotPassword'>
@@ -60,14 +67,14 @@ const Login = () => {
 						</div>
 
 						<div className='flex items-center justify-center'>
-							<Link >
-							<Button
-								type='submit'
-								mt='25px'
-								className=' !bg-secondary hover:!bg-secondary/50 transition-all duration-200 ease-in-out'
-							>
-								Login
-							</Button>
+							<Link>
+								<Button
+									type='submit'
+									mt='25px'
+									className=' !bg-secondary hover:!bg-secondary/50 transition-all duration-200 ease-in-out'
+								>
+									Login
+								</Button>
 							</Link>
 						</div>
 					</form>
--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -2,70 +2,88 @@ import React from 'react';
 import { useForm } from '@mantine/form';
 import { TextInput, Button } from '@mantine/core';
 import { Link } from '@tanstack/react-router';
-import { BsEye  , BsEyeSlash} from 'react-icons/bs';
+import { BsEye, BsEyeSlash } from 'react-icons/bs';
 import { useState } from 'react';
 const Signup = () => {
-
-	const [isEyeOpen , setIsEyeOpen] = useState(false);
-	const [isConfrimOpen , setIsConrfirmOpen] = useState(false);
+	const [isEyeOpen, setIsEyeOpen] = useState(false);
+	const [isConfrimOpen, setIsConrfirmOpen] = useState(false);
 	const form = useForm({
 		mode: 'uncontrolled',
-		initialValues: { password: '', email: '' , username: '' , confirmPassword : '' },
+		initialValues: {
+			password: '',
+			email: '',
+			username: '',
+			confirmPassword: '',
+		},
 
 		// Validation functions for each field
 		validate: {
-			username: (value) => (value.length <=3 ? 'Username should have at least 3 letters' : null),
+			username: (value) =>
+				value.length <= 3 ? 'Username should have at least 3 letters' : null,
 			password: (value) =>
 				value.length < 6 ? 'Password should have at least 6 letters' : null,
 			email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
-			confirmPassword: (value: string): string | null => (value !== form.values.password ? 'Passwords do not match' : null),
+			confirmPassword: (value: string): string | null =>
+				value !== form.values.password ? 'Passwords do not match' : null,
 		},
 	});
 
 	return (
 		<div className='min-h-screen flex items-center justify-center font-text-display'>
-			<div className='bg-card min-w-[350px] min-h-[530px] rounded-md px-10 border border-secondary/50'>
+			<div className='bg-card min-w-[350px] min-h-[500px] rounded-md px-10 border border-secondary/50'>
 				<div className='mt-4'>
 					<h1 className='text-2xl font-bold text-textBlack'>
 						Signup for new account
 					</h1>
 				</div>
 
-				<div className='mt-7'>
+				<div className='mt-4'>
 					<form onSubmit={form.onSubmit(console.log)}>
-						
 						<TextInput
 							mt='sm'
 							label='Username'
 							placeholder='Username'
 							key={form.key('username')}
-
 							{...form.getInputProps('username')}
-
 						/>
 						<TextInput
 							mt='sm'
 							label='Email'
 							placeholder='email'
 							key={form.key('email')}
-
 							{...form.getInputProps('username')}
-
 						/>
 						<TextInput
 							label='Password'
 							type={isEyeOpen ? 'text' : 'password'}
 							placeholder='Password'
-						   rightSection={<div onClick={() => setIsEyeOpen(!isEyeOpen)}>{isEyeOpen ? <BsEy className='text-black' /> : <BsEyeSlash className='text-black' />}</div>}
+							rightSection={
+								<div
+									className='cursor-pointer'
+									onClick={() => setIsEyeOpen(!isEyeOpen)}
+								>
+									{isEyeOpen ?
+										<BsEye className='text-black' />
+									:	<BsEyeSlash className='text-black' />}
+								</div>
+							}
 							mt='10px'
 							key={form.key('password')}
 							{...form.getInputProps('password')}
 						/>
 
-
-							<TextInput
+						<TextInput
 							label='Confirm Password'
-							  rightSection={<div onClick={() => setIsConrfirmOpen(!isConfrimOpen)}>{isConfrimOpen ? <BsEye className='text-black' /> : <BsEyeSlash className='text-black' />}</div>}
+							rightSection={
+								<div
+									className='cursor-pointer'
+									onClick={() => setIsConrfirmOpen(!isConfrimOpen)}
+								>
+									{isConfrimOpen ?
+										<BsEye className='text-black' />
+									:	<BsEyeSlash className='text-black' />}
+								</div>
+							}
 							placeholder='Confirm Password'
 							mt='10px'
 							key={form.key('confirmPassword')}
