d4e04506f6888ac33ef13e03a78722e2dd8b82a9
Albert534 <crustingenius@gmail.com>
2025-08-19 23:52:12 +0630

    job information

--- src/components/Job/EditModal.tsx
+++ src/components/Job/EditModal.tsx
@@ -1,28 +1,81 @@
-import React from 'react'
-import { Button } from '@mantine/core'
-import { InputWrapper, Modal , Input, Select } from '@mantine/core'
-const EditModal = ({opened , close , id} : {opened : boolean , close : () => void , id : number}) => {
-  return (
-   <>
-   <Modal opened={opened} onClose={close} title= {<div className='text-lg font-semibold mt-1'>Edit Job Information</div>} >
-           <div className='flex justify-between gap-3'>
-             <InputWrapper label="Job Title" required><Input className=''></Input></InputWrapper>
-              <InputWrapper label="Salary" required><Input className=''></Input></InputWrapper>
-           </div>
-   
-   <div className='flex justify-between gap-3 mt-4'>
-             <InputWrapper label="Experience" required><Select data={experiences} className=''></Select></InputWrapper>
-              <InputWrapper label="Status" required><Select data={['Active' , 'Inactive']} className=''></Select></InputWrapper>
-           </div>
-   
-   <div className='mt-6 flex justify-center '>
-     <Button className='!bg-secondary hover:!bg-secondary/80 transition-all duration-200 !text-md !border-none !text-white !px-2'>Add Job Information</Button>
-   </div>
-   
-   
-         </Modal>
-   </>
-  )
-}
+import React from 'react';
+import { Button } from '@mantine/core';
+import { InputWrapper, Modal, Input, Select } from '@mantine/core';
+const EditModal = ({
+	opened,
+	close,
+	id,
+}: {
+	opened: boolean;
+	close: () => void;
+	id: number;
+}) => {
+	const experiences = [
+		'Half Year',
+		'1 Year',
+		'2 Years',
+		'3 Years',
+		'4 Years',
+		'5 Years',
+		'6 Years',
+		'7 Years',
+		'8 Years',
+		'9 Years',
+		'10+ Years',
+	];
+	return (
+		<>
+			<Modal
+				opened={opened}
+				onClose={close}
+				title={
+					<div className='text-lg font-semibold mt-1'>Edit Job Information</div>
+				}
+			>
+				<div className='flex justify-between gap-3'>
+					<InputWrapper
+						label='Job Title'
+						required
+					>
+						<Input className=''></Input>
+					</InputWrapper>
+					<InputWrapper
+						label='Salary'
+						required
+					>
+						<Input className=''></Input>
+					</InputWrapper>
+				</div>
 
-export default EditModal
+				<div className='flex justify-between gap-8 mt-4'>
+					<InputWrapper
+						label='Experience'
+						required
+					>
+						<Select
+							data={experiences}
+							className=''
+						></Select>
+					</InputWrapper>
+					<InputWrapper
+						label='Status'
+						required
+					>
+						<Select
+							data={['Active', 'Inactive']}
+							className=''
+						></Select>
+					</InputWrapper>
+				</div>
+
+				<div className='mt-6 flex justify-center '>
+					<Button className='!bg-secondary hover:!bg-secondary/80 transition-all duration-200 !text-md !border-none !text-white !px-2'>
+						Edit Job Information
+					</Button>
+				</div>
+			</Modal>
+		</>
+	);
+};
+
+export default EditModal;
